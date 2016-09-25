8b07b1a0bf99ac8570dd5439fa1bcd4fdeaf7ac7
engmg2011 <eng.mg2011@gmail.com>
2016-09-25 13:31:05 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -39,34 +39,33 @@ angular.module('starter', ['ionic'])
   }];
  
   $scope.zoomMin = 1;
-  
+    
+  $scope.showImages = function(index) {
+    $scope.activeSlide = index;
+    $scope.showModal('templates/gallery-zoomview.html');
+  };
+   
+  $scope.showModal = function(templateUrl) {
+    $ionicModal.fromTemplateUrl(templateUrl, {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+      $scope.modal.show();
+    });
+  }
+   
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+    $scope.modal.remove()
+  };
+   
+  $scope.updateSlideStatus = function(slide) {
+    var zoomFactor = $ionicScrollDelegate.$getByHandle('scrollHandle' + slide).getScrollPosition().zoom;
+    if (zoomFactor == $scope.zoomMin) {
+      $ionicSlideBoxDelegate.enableSlide(true);
+    } else {
+      $ionicSlideBoxDelegate.enableSlide(false);
+    }
+  };
 });
 
-
-$scope.showImages = function(index) {
-  $scope.activeSlide = index;
-  $scope.showModal('templates/gallery-zoomview.html');
-};
- 
-$scope.showModal = function(templateUrl) {
-  $ionicModal.fromTemplateUrl(templateUrl, {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-    $scope.modal.show();
-  });
-}
- 
-$scope.closeModal = function() {
-  $scope.modal.hide();
-  $scope.modal.remove()
-};
- 
-$scope.updateSlideStatus = function(slide) {
-  var zoomFactor = $ionicScrollDelegate.$getByHandle('scrollHandle' + slide).getScrollPosition().zoom;
-  if (zoomFactor == $scope.zoomMin) {
-    $ionicSlideBoxDelegate.enableSlide(true);
-  } else {
-    $ionicSlideBoxDelegate.enableSlide(false);
-  }
-};
